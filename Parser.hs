9accbb8630fc6f960b392f371526d8ac8c426454
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2020-10-10 20:38:34 +0545

    Function Invocation is parsed and power operator added

--- Parser.hs
+++ Parser.hs
@@ -19,6 +19,7 @@ data Expression = IntNode Integer
                 | LogicalBinOp LogicalOp Expression Expression
                 | CmpNode CmpOp Expression Expression
                 | Id String
+                | Invoke String Expression -- function invocation. string followed by a tuple. parsed with parseFuncInvocation
                 | List [Expression]
                 | Assign Expression Expression -- the first Expression can only be of type Id String)
                 | Tuple [Expression]
@@ -27,24 +28,35 @@ data Expression = IntNode Integer
                 | Multiplication Expression Expression
                 | Division Expression Expression
                 | Modulus Expression Expression
+                | Power Expression Expression
                 | Negation Expression deriving (Show,Read)
 
 data Statement = PrintStatement Expression 
                | Eval Expression
     deriving (Show,Read)
 
--- data FunctionBody = FunctionBody Expression Expression  deriving (Show,Read) --  
+data FunctionBody = FunctionBody Expression Expression  deriving (Show,Read) -- the 2nd Expression after the 
+-- FunctionBody constructor should technically be just a Tuple
+-- Tuple [Expression] was included in Expression solely for this purpose
+-- We'll take care of this sort of weird representation of FunctionBody by using only
+-- the parseTuple for parsing the 1st expression
 
-
+--------------------------------------------------------------------------------------
+-- this pretty much sets up identifier grammar and some other stuff as well
+-- the tokens are haskellStyle tokens
 lexer :: TokenParser()
-lexer = makeTokenParser (haskellStyle {opStart = oneOf "+-*/%|&~=<>"
-                                   , opLetter = oneOf "+-*/%|&~=<>"})
+lexer = makeTokenParser (haskellStyle {opStart = oneOf "^+-*/%|&~=<>"
+                                   , opLetter = oneOf "^+-*/%|&~=<>"})
 
 
+-- uses the table and parseTerm function below to parse the expression 
 parseExpr :: Parser Expression
 parseExpr = buildExpressionParser table parseTerm
 
 -- for getting associativity right
+-- parses the expression correctly with the specified operators
+-- God bless the people who did this
+-- Cuz it wouldve been an absolute pain
 table = [[binary "=" (Assign) AssocRight]
         ,[binary "==" (CmpNode Eq) AssocNone,binary "/=" (CmpNode NEq) AssocNone
         ,binary "<" (CmpNode Lt) AssocNone , binary "<=" (CmpNode Lte) AssocNone
@@ -53,6 +65,7 @@ table = [[binary "=" (Assign) AssocRight]
         ,[binary "&&"(LogicalBinOp And) AssocRight, binary "||" (LogicalBinOp Or) AssocRight
          ,binary "->" (LogicalBinOp Implies) AssocLeft , binary "<->" (LogicalBinOp DoubleImplies)  AssocLeft]
         ,[prefix "-" (Negation), prefix "+" (id) ]
+        ,[binary "^" (Power) AssocLeft]
         ,[binary "*" (Multiplication) AssocLeft, binary "/" (Division) AssocLeft,
          binary "%" (Modulus) AssocLeft]
         ,[binary "+" (Addition) AssocLeft, binary "-" (Subtraction) AssocLeft]]
@@ -79,20 +92,33 @@ parseString = do
         "False" -> BoolNode False
         otherwise -> Id str
 
+-- parses function invocation
+-- parses a string 
+-- then a tuple
+parseFuncInvocation :: Parser Expression
+parseFuncInvocation = do
+    funcName <- identifier lexer
+    arguments <- parens lexer parseTuple
+    return $ Invoke funcName arguments
+
+
 -- this parses term 
 -- expr is something like  term (operation) term
 -- term can also be an expression enclosed inside parens
---
+-- Im leaving this only as a definition of a term
+-- I couldve done parens lexer parseExpression but since i dont have array 
+-- operations ,such as + and all ,defined ,I chose not to include them
 parseTerm :: Parser Expression
 parseTerm = parens lexer parseExpr
             <|> parseNumber 
+            <|> (try $ parseFuncInvocation)
             <|> parseString
 
 
 
 
 parseExprList :: Parser Expression
-parseExprList = liftM List $ sepBy parseTerm (comma lexer) --liftM makes the List Constructor Monadic
+parseExprList = liftM List $ sepBy parseExpression (comma lexer) --liftM makes the List Constructor Monadic
 {- this is the same as above
 parseExprList = do
     arr <- sepBy parseTerm (comma lexer)
@@ -100,21 +126,23 @@ parseExprList = do
 -}
 -- same thing as above going on in parseTuple
 parseTuple :: Parser Expression
-parseTuple = liftM Tuple $ sepBy parseTerm (comma lexer)
+parseTuple = liftM Tuple $ sepBy parseExpression (comma lexer)
 
 
 --  top level parser for Expression type
 --  combines tuple parser,list parser and normal expression parser
---
+-- Im not parsing for a tuple in expression
+-- Because i plan to use the Tuple [Expression] only for function definitions and 
+-- invocations
 parseExpression :: Parser Expression
-parseExpression = do
+parseExpression = do 
     s <- (try $ parseExpr)
-        <|> ( try $ parens lexer parseTuple)
+        <|> (try $ parens lexer $ brackets lexer parseExprList)
         <|> brackets lexer parseExprList
     return s
 
 -- parses a print statement
--- print x
+-- print x and something like that
 -- used when you want to print the output of an evaluation
 parsePrint :: Parser Statement
 parsePrint = do
@@ -128,7 +156,6 @@ parseEvalStatement :: Parser Statement
 parseEvalStatement = do
     whiteSpace lexer
     s <- parseExpression
-    eof
     return $ Eval s
 
 -- parses statement
